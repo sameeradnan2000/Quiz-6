362e2b76145277e4b4525299b9126e897ad39b8c
Sameer Adnan <60285414+sameeradnan2000@users.noreply.github.com>
2022-04-29 22:42:50 +0500

    Update tower.py

--- tower.py
+++ tower.py
@@ -1,2 +1,15 @@
+from re import M
+
+
 def solve(X: int) -> [int, int]:
-    pass
+    lst = list()
+    m = X
+    for i in range(1,X):
+        TotalArea = i*i*i
+        if TotalArea < X:
+            lst.append(i)
+            cube += TotalArea
+    max = sum(lst)
+    print (max + cube)
+
+solve()
